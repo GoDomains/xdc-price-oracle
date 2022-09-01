63572ea55071381f0e7105ce8dba7175171bc397
Nitish Patel <nitishp74@gmail.com>
2022-09-01 14:35:10 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Container, Typography, Card } from '@mui/material';
+
+function App() {
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Card sx={{ p: 4 }}>
+        <Typography variant="h4">Hello Yodaplus!</Typography>
+      </Card>
+    </Container>
+  );
+}
+
+export default App;
