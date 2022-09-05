70caa5c4e352def45279ae84ee6f681e4b3e191f
Shadab Akhtar <akhtarshadab415@gmail.com>
2022-09-05 12:56:57 +0530

    remove staticfiles on prod - No nginx

--- docker/backend/run.prod.sh
+++ docker/backend/run.prod.sh
@@ -3,8 +3,8 @@
 ./docker/backend/run.sh
 
 echo "==> $(date +%H:%M:%S) ==> Collecting statics... "
-rm -rf /static/*
-cp -r staticfiles/ /static/
+# rm -rf /static/*
+# cp -r staticfiles/ /static/
 
 echo "==> $(date +%H:%M:%S) ==> Running uWSGI server... "
 uwsgi --ini uwsgi.ini
